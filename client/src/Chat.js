3e840b4e7a8bd3e3b2c22a6aa78aba4b42fa756e
Rithik1010 <rithikroshan018@gmail.com>
2022-08-22 14:05:52 +0530

    Add Styling

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -23,7 +23,7 @@ function Chat({ socket, username, room }) {
     }, [socket]);
 
     return (
-        <div>
+        <div className="chat-window">
             <div className="chat-header">
                 <p>Live Chat</p>
             </div>
